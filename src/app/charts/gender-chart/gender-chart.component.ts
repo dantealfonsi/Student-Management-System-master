74488d378dd13c82dbc61cf3ee41cfec473a00ba
kakolukiyan <daniel.alfonsibusiness@gmail.com>
2024-12-03 14:23:25 -0400

    [ADD] PRINT REPORT

--- src/app/charts/gender-chart/gender-chart.component.ts
+++ src/app/charts/gender-chart/gender-chart.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, SimpleChanges, ViewChild, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, ViewChild, OnInit, OnChanges, ElementRef } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToggleSwitchComponent } from 'src/assets/toggle-switch/toggle-switch.component';
 import { CategoryScale, Chart, ChartConfiguration, LinearScale, ChartData, ChartEvent, Colors, Legend, ArcElement, DoughnutController } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { provideCharts } from 'ng2-charts';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 Chart.register(CategoryScale, LinearScale, ArcElement, DoughnutController);
 
@@ -98,4 +100,67 @@ export class GenderChartComponent implements OnInit, OnChanges {
   }): void {
     console.log(event, active);
   }
+
+
+
+  @ViewChild('pdfContent') pdfElement!: ElementRef;
+
+
+  generatePDF() {
+    const pdfContent = this.pdfElement.nativeElement;
+    const exportButton = document.querySelector('.export');
+
+    // Ocultar el botón
+    if (exportButton) {
+      exportButton.classList.add('hidden');
+    }
+
+    html2canvas(pdfContent, { scale: 2, backgroundColor: '#FFFFFF' }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const doc = new jsPDF({
+        orientation: 'landscape',
+        unit: 'pt',
+        format: 'letter'
+      });
+
+      const margin = 30;
+      const marginY = 180; // Ajuste de margen superior
+
+      // Añadir la imagen del escudo y los encabezados al PDF
+      const img = new Image();
+      img.src = '../../assets/img/JFB_LOGO_PURPLE.png'; // Cambia esto a la ruta real de tu imagen
+      
+      img.onload = () => {
+        doc.addImage(img, 'PNG', 80, 40, 90, 90); // Aumentar tamaño de la imagen
+        doc.setFontSize(16);
+        doc.setTextColor(40, 40, 40); // Color del texto del primer encabezado
+        doc.text('Unidad Educativa José Francisco Bermúdez', 180, 80);
+
+        doc.setFontSize(18);
+        doc.setTextColor(0, 0, 0); // Color del texto de "Reportes"
+        doc.text('Reportes: Historial de Usuario', 180, 100);
+
+        // Ajustar el tamaño y posición del contenido capturado en el PDF
+        const scale = 0.60; // Ajusta este valor para hacer el contenido un poco más ancho
+        const pdfWidth = (doc.internal.pageSize.getWidth() + 12 * margin) * scale;
+        const pdfHeight = (doc.internal.pageSize.getHeight() - 70 - margin) * scale;
+        const xOffset = (doc.internal.pageSize.getWidth() - pdfWidth) / 2; // Centrar horizontalmente
+
+        // Añadir el contenido capturado al PDF
+        doc.addImage(imgData, 'PNG', xOffset, marginY, pdfWidth, pdfHeight);
+
+        // Guardar el PDF
+        doc.save('reporte_estudiantes.pdf');
+
+        // Mostrar el botón nuevamente
+        if (exportButton) {
+          exportButton.classList.remove('hidden');
+        }
+      };
+    });
+  }
+
+
 }
+
+
